b2fce2526b9fcec827401f6be12815d35ef99fa5
armin azhdehnia <arminmk18@gmail.com>
2021-09-27 07:47:16 -0400

    add new manager.py

---
+++ manager.py
@@ -0,0 +1,18 @@
+import os
+import argparse
+from argparse import Namespace
+
+
+def main(arguments: Namespace):
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # default options
+    # yolo options
+    # mono depth options
+    # PyD-net options
+    args = parser.parse_args()
+
+    main(arguments=args)
